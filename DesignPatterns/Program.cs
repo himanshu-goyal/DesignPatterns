fc1b870d2236caebcd7d89170cac1bed776cc377
himanshu-goyal <himanshu.goyal@outlook.com>
2019-02-06 11:52:01 -0600

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonPattern;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter one of the chars from following to explore the design pattern");
+            char value = Convert.ToChar(Console.ReadLine());
+            switch (value)
+            {
+                case 'S':
+                    Singleton objectA = Singleton.GetInstance;
+                    objectA.PrintDetails("This is from Object A");
+                    Singleton objectB = Singleton.GetInstance;
+                    objectA.PrintDetails("This is from Object B");
+                    break;
+                case 'D':
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        //Private prop as this will be only initiized with null and make sure only one instance is there.
+        private static Singleton instance=null;
+        private static int counter = 0;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if(instance==null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        
+        //Private construct, this will give us number of object initilized.
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Constructor called,Counter value: " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //This class was created to understand why we need to seal our base class.
+        //We will create an instance of derived class and we will see that counter will increase to 2
+        //That will viloate out singleton pricipal. 
+        //public class DerivedSingleton : Singleton
+        //{
+
+        //}
+    }
+}
